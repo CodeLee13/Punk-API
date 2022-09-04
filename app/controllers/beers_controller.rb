e233356cae416b229be707c26c1636957886f2c1
leonpeterkin <leonpeterkin@icloud.com>
2022-09-04 14:48:11 -0400

    Dry Principle Implimented

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -8,9 +8,7 @@ class BeersController < ActionController::API
             all_beers = []
             BASE_URLS_ARRAY.each do |url|
                 uri = URI(url+"/#{id}")
-                res = Net::HTTP.get_response(uri)
-                beers = JSON.parse(res.body)
-                all_beers << beers.map{ |b| Beer.new(b) }
+                all_beers = make_api_call(uri)
             end
             render json: { data: all_beers }
         rescue Exception => e
@@ -24,9 +22,7 @@ class BeersController < ActionController::API
             all_beers = []
             BASE_URLS_ARRAY.each do |url|
                 uri = URI(url)
-                res = Net::HTTP.get_response(uri)
-                beers = JSON.parse(res.body)
-                all_beers << beers.map{ |b| Beer.new(b) }
+                all_beers << make_api_call(uri)
             end
             render json: { data: all_beers }
         rescue Exception => e
@@ -40,13 +36,20 @@ class BeersController < ActionController::API
             all_beers = []
             BASE_URLS_ARRAY.each do |url|
                 uri = URI(url+"?beer_name=#{name}")
-                res = Net::HTTP.get_response(uri)
-                beers = JSON.parse(res.body)
-                all_beers << beers.map{ |b| Beer.new(b) }
+                all_beers << make_api_call(uri)
             end
             render json: { data: all_beers }
         rescue Exception => e
             render json: e.message
         end
+    end
+
+    private
+
+    def make_api_call(url)
+        res = Net::HTTP.get_response(url)
+        response = JSON.parse(res.body)
+        response = response.map { |b| Beer.new(b) }
+        response
     end
 end
